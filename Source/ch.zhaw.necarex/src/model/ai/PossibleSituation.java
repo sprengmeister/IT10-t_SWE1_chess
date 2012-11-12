17c6dc21009d1636d0317055d67a6e5538522c50
florianbosshard <florian.bosshard@gmail.com>
2012-11-12 21:56:52 +0100

    Attribute geändert

--- Source/ch.zhaw.necarex/src/model/ai/PossibleSituation.java
+++ Source/ch.zhaw.necarex/src/model/ai/PossibleSituation.java
@@ -1,10 +1,14 @@
 package model.ai;
 
+import model.ChessBoard;
+
 /**
  * Stellt eine mögliche Schachsituation dar. Diese Klasse nimmt die Bewertung der Schachsituation vor. 
  * @author florian
  *
  */
 public class PossibleSituation {
+	private ChessBoard board;
+	private int score;
 
 }
