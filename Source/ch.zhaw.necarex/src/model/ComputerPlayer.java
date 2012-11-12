27958ec22a34f8bee8743230f813939e53c05e51
florianbosshard <florian.bosshard@gmail.com>
2012-11-12 10:02:18 +0100

    Klassen-Stubs für AI hinzugefügt

---
+++ Source/ch.zhaw.necarex/src/model/ComputerPlayer.java
@@ -0,0 +1,14 @@
+package model;
+/**
+ * Stellt einen Computerspieler dar. Benutzt für die Zugberechnung das Package model.ai
+ * @author florian
+ *
+ */
+public class ComputerPlayer extends Player {
+
+	public ComputerPlayer(PlayerColor color) {
+		super(color);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Source/ch.zhaw.necarex/src/model/ai/PossibleSituation.java
@@ -0,0 +1,10 @@
+package model.ai;
+
+/**
+ * Stellt eine mögliche Schachsituation dar. Diese Klasse nimmt die Bewertung der Schachsituation vor. 
+ * @author florian
+ *
+ */
+public class PossibleSituation {
+
+}
