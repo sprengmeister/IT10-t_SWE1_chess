6478f3fea628886079c27784a958753f903d2876
Dae Beni <hohlben@students.zhaw.ch>
2012-11-20 15:04:36 +0100

    Pferd (knight) auscodiert

--- Source/ch.zhaw.necarex/src/model/pieces/Knight.java
+++ Source/ch.zhaw.necarex/src/model/pieces/Knight.java
@@ -5,18 +5,18 @@
 package model.pieces;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
 import model.*;
 
 /**
- * Spielfigur Bauer, basiert auf Piece. Kennt die Gangart des und kann dessen mögliche Felder ausrechnen.
- * @author florian
+ * Spielfigur Pferd, basiert auf Piece. Kennt die Gangart des und kann dessen mögliche Felder ausrechnen.
+ * @author beni
  */
 public class Knight extends Piece {
-    
-    private int direction;
 
     /**
-     * Konstruktor: Erzeugt eine neue Figur
+     * Konstruktor: Erzeugt einen Pferd
      * @param player Referenz zum Spieler 
      * @param chessBoard Referenz zum Schachbrett 
      */
@@ -32,13 +32,52 @@ public class Knight extends Piece {
     public ArrayList<ChessField> getPossibleFields() {
     	ArrayList<ChessField> possibleFields = new ArrayList<ChessField>();
     
-
-
+    	//alle hinzufügen, danach unmögliche löschen
+        this.addAllKnightTurn(possibleFields);
+        this.removeImpossibleKnightTurn(possibleFields);
 
         //TODO prüfen ob mit diesem Zug eine Schachsituation ausgelöst wurde
         
         return possibleFields;
     }
-
     
+    /**
+     * Prüft, ob das Pferd ein Feld nach vorne fahren kann. 
+     */
+    private void addAllKnightTurn(ArrayList<ChessField> possibleFields){
+    	possibleFields.add(this.getChessBoard().getField((this.getChessField().getCol() + 2), (this.getChessField().getRow() + 1)));
+    	possibleFields.add(this.getChessBoard().getField((this.getChessField().getCol() + 2), (this.getChessField().getRow() - 1)));
+    	possibleFields.add(this.getChessBoard().getField((this.getChessField().getCol() - 2), (this.getChessField().getRow() + 1)));
+    	possibleFields.add(this.getChessBoard().getField((this.getChessField().getCol() - 2), (this.getChessField().getRow() - 1)));
+    	possibleFields.add(this.getChessBoard().getField((this.getChessField().getCol() + 1), (this.getChessField().getRow() + 2)));
+    	possibleFields.add(this.getChessBoard().getField((this.getChessField().getCol() + 1), (this.getChessField().getRow() - 2)));
+    	possibleFields.add(this.getChessBoard().getField((this.getChessField().getCol() - 1), (this.getChessField().getRow() + 2)));
+    	possibleFields.add(this.getChessBoard().getField((this.getChessField().getCol() - 1), (this.getChessField().getRow() - 2)));
+    }
+    
+    /**
+     * Prüft, ob das Pferd schlagen kann
+     */
+    private void removeImpossibleKnightTurn(ArrayList<ChessField> possibleFields){
+    	//Figur steht ausserhalb des Schachbretts
+    	Iterator<ChessField> outsideChessBoard = possibleFields.iterator();
+    	while (outsideChessBoard.hasNext()) {
+    		ChessField currentChessfield = outsideChessBoard.next();
+    		int col = currentChessfield.getCol();
+    		int row = currentChessfield.getRow();
+    		if(col > 8 || row > 8 || col < 0 || row < 0){
+    			outsideChessBoard.remove();
+    		}
+    	}
+    	
+    	
+    	//Figur würde andere Figur der gleichen Farbe schlagen
+    	Iterator<ChessField> samePlayer = possibleFields.iterator();
+    	while (samePlayer.hasNext()) {
+    		ChessField currentChessfield = samePlayer.next();
+    		if(currentChessfield.getPiece() != null && currentChessfield.getPiece().getOwner() == this.getChessField().getPiece().getOwner()){
+    			samePlayer.remove();
+    		}
+    	}
+    }    
 }
