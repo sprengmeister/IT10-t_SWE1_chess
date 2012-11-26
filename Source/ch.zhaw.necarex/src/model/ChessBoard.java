89f2aee461df51d60522131c6d787d27250667ea
sprengmeister <sebastian.sprenger@outlook.com>
2012-11-26 13:14:57 +0100

    getInitialPiece wieder auf vorherigen Stand zurück gesetzt, wo alle Figuren gesetzt werden

--- Source/ch.zhaw.necarex/src/model/ChessBoard.java
+++ Source/ch.zhaw.necarex/src/model/ChessBoard.java
@@ -4,9 +4,12 @@
  */
 package model;
 
+import model.pieces.BiShop;
+import model.pieces.King;
 import model.pieces.Knight;
 import model.pieces.Pawn;
 import model.pieces.Piece;
+import model.pieces.Queen;
 import model.pieces.Rook;
 
 /**
@@ -25,64 +28,50 @@ public class ChessBoard {
     } 
     
     private Piece getInitialPiece(int col, int row){
-        Player player;
+    	Player player;
         if(row < 3 ){
             player = game.getPlayerWhite();
         } else {
             player = game.getPlayerBlack();
         }
-        // Zeile 1, Weisse Grundlinie
-        if(row == 0){
-            // Türme weiss
-            if(col == 0 || col == 7){
-            	return new Rook(player, this);   
-            }
-            // Springer weiss
-            if(col == 1 || col == 6){
-            	return new Knight(player, this);                
-            }
-            // Läufer weiss
-            if(col == 2 || col == 5){
-                
-            }
-            // Dame weiss
-            if(col == 3){
-                
-            }
-            // König weiss
-            if(col == 4){
-                
-            }
-        }
-        // Zeile 7, Schwarze Grundlinie
-        if(row == 7){
-            // Türme schwarz
-            if(col == 0 || col == 7){
-            	return new Rook(player, this);  
-            }
-            // Springer schwarz
-            if(col == 1 || col == 6){
-            	return new Knight(player, this);
-            }
-            // Läufer schwarz
-            if(col == 2 || col == 5){
-                
-            }
-            // König schwarz
-            if(col == 3){
-                
-            }
-            // Dame schwarz
-            if(col == 4){
-                
-            }
+        
+        //Aus Row/Column Werten eine Zahl machen -> Zehner = Row, Einer = Column
+        int fieldValue = row*10+col;
+        // Reihe 0 -> Weisse Grundlinie 
+        // Reihe 7 -> Schwarze Grundlinie
+        switch (fieldValue){
+	        case 0:
+	        case 7:
+	        case 70:
+	        case 77:
+	        	// Türme
+	        	return new Rook(player, this);
+	        case 1:
+	        case 6:
+	        case 71:
+	        case 76:
+	        	// Springer
+	        	return new Knight(player, this);
+	        case 2:
+	        case 5:
+	        case 72:
+	        case 75:
+	        	// Läufer
+	        	return new BiShop(player, this);
+	        case 3:
+	        case 73:
+	        	// Dame
+	        	return new Queen(player, this);
+	        case 4:
+	        case 74:
+	        	// König
+	        	return new King(player, this);
         }
         
-        // Bauern weiss und schwarz
+        // Bauern separat Abfragen (um sich die ganzen case-Statements zu sparen)
         if(row == 1 || row == 6 ){
             return new Pawn(player, this);
         }
-        
         
         return null;
     }
