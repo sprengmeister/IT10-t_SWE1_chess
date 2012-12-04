83281ca0f9762dbb482728b7e379eddb5a406508
florianbosshard <florian.bosshard@gmail.com>
2012-12-04 16:45:02 +0100

    Methoden ausgelagert, checkDaringOwnKing

--- Source/ch.zhaw.necarex/src/model/pieces/Rook.java
+++ Source/ch.zhaw.necarex/src/model/pieces/Rook.java
@@ -33,86 +33,28 @@ public class Rook extends Piece {
      */
     @Override
     public ArrayList<ChessField> getPossibleFields() {
-    	ArrayList<ChessField> possibleFields = new ArrayList<ChessField>();
-    
-    	ChessField targetField;
-    	
-    	//nach links fahren
-		if (this.getChessField().getCol() != 0) {
-			targetField = this.getChessBoard().getField(this.getChessField().getCol(), this.getChessField().getRow());
-			for (int i = 0; i < 7; i++){
-				if(targetField.getCol() - 1 < 0) break;			
-				targetField = this.getChessBoard().getField(targetField.getCol() - 1, targetField.getRow());	
-				if (targetField.getPiece() == null) {	
-					possibleFields.add(targetField);
-				} else {
-					checkForPiece(possibleFields, targetField);
-					break;
-				}
-			}				
-		}
-
-		//nach rechts fahren
-		if (this.getChessField().getCol() != 7) {
-			targetField = this.getChessBoard().getField(this.getChessField().getCol(), this.getChessField().getRow());
-			for (int i = 0; i < 7; i++){
-				if(targetField.getCol() + 1 > 7) break;
-				targetField = this.getChessBoard().getField(targetField.getCol() + 1, targetField.getRow());
-				if (targetField.getPiece() == null) {
-					possibleFields.add(targetField);
-				} else {
-					checkForPiece(possibleFields, targetField);
-					break;
-				}
-			}
-		}
-
-		//nach oben fahren
-		if (this.getChessField().getRow() != 7) {
-			targetField = this.getChessBoard().getField(this.getChessField().getCol(), this.getChessField().getRow());
-			for (int i = 0; i < 7; i++){
-				if(targetField.getRow() + 1 > 7) break;
-				targetField = this.getChessBoard().getField(targetField.getCol(), targetField.getRow() + 1);
-				if (targetField.getPiece() == null) {
-					possibleFields.add(targetField);
-				} else {
-					checkForPiece(possibleFields, targetField);
-					break;
-				}
-			}
-		}
-			
-		//nach unten fahren
-		if (this.getChessField().getRow() != 0) {
-			targetField = this.getChessBoard().getField(this.getChessField().getCol(), this.getChessField().getRow());
-			for (int i = 0; i < 7; i++){
-				if(targetField.getRow() - 1 < 0) break;
-				targetField = this.getChessBoard().getField(targetField.getCol(), targetField.getRow() - 1);
-				if (targetField.getPiece() == null) {
-					possibleFields.add(targetField);
-				} else {
-					checkForPiece(possibleFields, targetField);
-					break;
-				}
-			}
-		}
-        
-        //TODO prüfen ob mit diesem Zug eine Schachsituation ausgelöst wurde
-        
-        return possibleFields;
+    	possibleFields = new ArrayList<ChessField>();
+       	
+    	this.checkTurnColumn();
+    	this.checkTurnRow();
+ 
+    	return this.checkDaringOwnKing(possibleFields);
     }
+    
     @Override
     public ArrayList<ChessField> getDaringFields(){
-    	return this.getPossibleFields();
+    	possibleFields = new ArrayList<ChessField>();
+       	
+    	this.checkTurnColumn();
+    	this.checkTurnRow();
+ 
+    	return possibleFields;
     }
     
     
-    /**
-     * Ist die gefunde Figur vom Gegner oder nicht?
-     */
-    private void checkForPiece(ArrayList<ChessField> possibleFields, ChessField targetField){
-    	if (targetField.getPiece().getOwner() != this.getChessField().getPiece().getOwner()) {
-    		possibleFields.add(targetField);
-		}
-    }       
+    
+    
+    
+    
+       
 }
